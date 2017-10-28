96b1ce4acde6a6a4f081f07c37779b1fbc3ee211
Daniel Schmidt <silveirinha@gmail.com>
2017-10-28 15:11:56 -0200

    refactored Array#max_slice_sum

--- lib/max_sequence_sum.rb
+++ lib/max_sequence_sum.rb
@@ -1,17 +1,13 @@
 class Array
     def max_slice_sum
-        raise TypeError.new "Can't sum non-numeric values" unless self.all? {|e| e.is_a? Numeric}
+        raise TypeError.new "can't sum non-numeric values" unless self.all? {|e| e.is_a? Numeric}
 
-        if self.empty?
-            0
-        elsif self.all? {|e| e.positive?}
-            self.sum
-        else
-            # all subarrays
-            subarys = []
-            (1..self.length).each {|size| self.each_cons(size) {|subary| subarys << subary}}
-            # subarray with greater sum
-            subarys.max_by {|ary| ary.sum}.sum
-        end
+        return 0 if self.empty?
+
+        # generates all subarrays
+        subarys = []
+        (1..self.length).each {|size| self.each_cons(size) {|subary| subarys << subary}}
+        # returns subarray with largest sum
+        subarys.max_by {|ary| ary.sum}.sum
     end
 end
--- spec/max_sequence_sum_spec.rb
+++ spec/max_sequence_sum_spec.rb
@@ -3,57 +3,57 @@ require "max_sequence_sum"
 describe Array do
     describe "#max_slice_sum" do
         context "given a non-numeric array" do
+            array = ["a", "b", "c"]
             it "raises a TypeError exception" do
-                array = ["a", "b", "c"]
                 expect{(array.max_slice_sum)}.to raise_exception TypeError
             end
         end
 
         context "given an empty array" do
+            array = []
             it "returns 0" do
-                array = []
                 expect(array.max_slice_sum).to eql 0
             end
         end
 
         context "given an array of 1 element" do
+            array = [9]
             it "returns this 1 element" do
-                array = [9]
                 expect(array.max_slice_sum).to eql 9
             end
         end
 
-        context "given the array [1,2,3,4,5] with only positive numbers" do
+        context "given an array with only positive numbers" do
+            array = [1,2,3,4,5]
             it "returns 15" do
-                array = [1,2,3,4,5]
                 expect(array.max_slice_sum).to eql 15
             end
         end
 
-        context "given the array [-1,3,4,5,-6] with negative numbers on the sides" do
+        context "given an array with leading an trailing negative numbers" do
+            array = [-1,-2,3,4,5,-6,-7]
             it "returns 12" do
-                array = [-1,3,4,5,-6]
                 expect(array.max_slice_sum).to eql 12
             end
         end
 
-        context "given the array [1,-3,-4,5,6,-7] with positive and negative numbers intermixed" do
+        context "given an array with positive and negative numbers intermixed" do
+            array = [1,-3,-4,5,6,-7]
             it "returns 11" do
-                array = [1,-3,-4,5,6,-7]
                 expect(array.max_slice_sum).to eql 11
             end
         end
 
         context "given the array [1,-3,-4,5,2,-7,7,2,8,-1,2,-9,-2,-5] with max subarray [7,2,8,-1,2]" do
+            array = [1,-3,-4,5,2,-7,7,2,8,-1,2,-9,-2,-5]
             it "returns 18" do
-                array = [1,-3,-4,5,2,-7,7,2,8,-1,2,-9,-2,-5]
                 expect(array.max_slice_sum).to eql 18
             end
         end
 
-        context "given the array [-4,-3,-2,-5,-6,-7] with only negative numbers" do
+        context "given an array with only negative numbers" do
+            array = [-4,-3,-2,-5,-6,-7]
             it "returns -2" do
-                array = [-4,-3,-2,-5,-6,-7]
                 expect(array.max_slice_sum).to eql -2
             end
         end
