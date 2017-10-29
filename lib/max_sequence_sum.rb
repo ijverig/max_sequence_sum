c94978496c197fe674067652632a7f4c6dd30654
Daniel Schmidt <silveirinha@gmail.com>
2017-10-29 03:41:04 -0200

    more refactoring on Array#max_slice_sum - empty array case

--- lib/max_sequence_sum.rb
+++ lib/max_sequence_sum.rb
@@ -2,16 +2,14 @@ class Array
     def sum
         inject(:+)
     end
-    
+
     def max_slice_sum
         raise TypeError, "can't sum non-numeric values" unless all? {|e| e.is_a? Numeric}
-
-        return 0 if empty?
 
         # generates all subarrays
         subarys = []
         (1..length).each {|size| each_cons(size) {|subary| subarys << subary}}
-        # returns subarray with largest sum
-        subarys.max_by {|ary| ary.sum}.sum
+        # returns subarray largest sum or 0 if array is empty
+        subarys.max_by {|ary| ary.sum}&.sum || 0
     end
 end
